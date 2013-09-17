a9d599014b486c2f5ce1c6f325c56c73ec967195
Calvin Metcalf <calvin.metcalf@state.ma.us>
2013-09-16 20:58:17 -0400

    tikue's suggestions

--- simplify.rs
+++ simplify.rs
@@ -1,18 +1,15 @@
-#[link(name = "simplify", vers = "0.0.5")];
+#[link(name = "simplify", vers = "0.0.6")];
 extern mod extra;
 use extra::json::*;
 use extra::treemap::TreeSet;
 use std::vec;
+#[deriving(Clone, Eq)]
 pub struct Point {
 	x: float,
 	y: float
 }
 impl ToJson for Point {
 	fn to_json(&self) -> Json { List(~[self.x.to_json(),self.y.to_json()]) }
-}
-impl Eq for Point {
-	fn eq(&self, other: &Point) -> bool { other.x == self.x && other.y == self.y }
-	fn ne(&self, other: &Point) -> bool { other.x != self.x || other.y != self.y }
 }
 impl Sub<Point,Point> for Point {
 	#[inline]
@@ -34,46 +31,32 @@ impl Point {
 	fn mul(self, other: float) -> Point { Point { x:self.x * other, y:self.y * other }}
 	fn add(self, other: float) -> Point {  Point {x:self.x + other, y:self.y + other }}
 }
-struct Pair(uint,uint);
-enum Opt<T,U>{
-	OK(T,T,U),
-	NotOK
-}
-impl Pair {
-fn first(self)-> uint {
-		match self {
-			Pair(l,_)=>l
-		}
-	}
-	fn last(self)-> uint {
-		match self {
-			Pair(_,l)=>l
-		}
-	}
-}
+type Pair = (uint, uint);
 fn calcStuff(p:Point,p1:Point,d1:Point)->float {
-	let top : float = ((p - p1) * d1).sum();
-	let bottom : float =  d1.sqsum();
-	match bottom{
-		0.0=>0.0,
-		_=>top/bottom
+	let top = ((p - p1) * d1).sum();
+	let bottom =  d1.sqsum();
+	if bottom == 0.0 {
+	    0.0
+	}else{
+	    top/bottom
 	}
 }
 fn getSquareSegmentDistance(p: Point, p1: Point, p2: Point) -> float {
-	let d1 : Point = p2-p1;
-	let d2 : Point = match d1{
+	let d1 = p2-p1;
+	let d2 = match d1{
 		Point {x:0.0,_} | Point {y:0.0,_}=> {p1}
 		_=>{
-			let t : float = calcStuff(p,p1,d1);
-			match t {
-				tt if tt>1.0=>p2,
-				tt if tt>0.0=> d1.mul(tt)+p1,
-				_=>p1
+			let t = calcStuff(p,p1,d1);
+			if t>1.0 {
+			    p2
+			}else if t>0.0{
+			    d1.mul(t)+p1
+			}else{
+			    p1
 			}
 		}
 	};
-	let d4 : Point = p-d2;
-	d4.sqsum()
+	(p-d2).sqsum()
 }
 
 fn simplifyRadialDistance(points:~[Point], sqTolerance:float) -> ~[Point]{
@@ -95,48 +78,49 @@ fn simplifyRadialDistance(points:~[Point], sqTolerance:float) -> ~[Point]{
 }
 
 fn simplifyDouglasPeucker(points : ~[Point], tolerance : float) -> ~[Point]{
-	let len : uint = points.len();
+	let len = points.len();
 	let mut markers = TreeSet::new();
 	let mut stack : ~[Pair] = ~[];
 	markers.insert(0u);
 	markers.insert(len-1u);
-	let mut pair = Pair(0u,len-1u);
-	loop{
-		let first = pair.first();
-		let last = pair.last();
-		let mut index : uint = 0;
-		let mut maxSqDist : float = 0.0f;
-		let mut i : uint = first + 1u;
-		while (i < last) {
-			let sqDist :float  = getSquareSegmentDistance(
-				points[i], 
-				points[first], 
-				points[last]
-			);
-			if (sqDist > maxSqDist) {
-				index = i;
-				maxSqDist = sqDist;
-			}
-			i += 1;
-		}
-		if maxSqDist > tolerance {
-				markers.insert(index);
-				stack.push(Pair(first,index));
-				stack.push(Pair(index,last));
-		}
-		match stack.pop_opt() {
-			Some(p)=>pair=p,
-			None=>break
-		};
-	};
+	let mut pair:Pair = (0u,len-1u);
+	loop {
+        let first = pair.first();
+        let second = pair.second();
+        let (first_pt, second_pt) = (points[first], points[second]);
+        let mut index = 0;
+        let mut max_sq_dist = 0.0f;
+        let i = first + 1u;
+        for (i, &point_i) in points.slice_from(i)
+            .iter()
+            .enumerate()
+            .map(|(new_i, point)| (i + new_i, point))
+            .take_while(|&(i, _)| i < second) {
+            let sq_dist = getSquareSegmentDistance(point_i, first_pt, second_pt);
+            if (sq_dist > max_sq_dist) {
+                index = i;
+                max_sq_dist = sq_dist;
+            }
+        }
+        if max_sq_dist > tolerance {
+            markers.insert(index);
+            stack.push((first,index));
+            stack.push((index,second));
+        }
+        match stack.pop_opt() {
+            Some(p)=>pair=p,
+            None=>break
+        };
+    }
 	vec::from_fn(markers.len(),|k| points[k])
 }
 
 pub fn simplify(points : ~[Point], sqTolerance : float, hq:bool) -> ~[Point]{
-	let tolerance : float = sqTolerance*sqTolerance;
-	let pts:~[Point]=match hq {
-		true=>points,
-		_=>simplifyRadialDistance(points,tolerance)
+	let tolerance = sqTolerance*sqTolerance;
+	let pts:~[Point] = if hq {
+	    points
+	} else {
+	    simplifyRadialDistance(points,tolerance)
 	};
 	simplifyDouglasPeucker(pts,tolerance)
 }
